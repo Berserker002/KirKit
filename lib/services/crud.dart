aeb4dae93e047037fdb1130f204397f7b5119f53
itspyro <hpandeycsebt18@gmail.com>
2020-10-20 21:23:16 +0530

    Added uploading data to firebase.

---
+++ lib/services/crud.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class CrudMethods {
+  Future<void> addData (blogData) async{
+    await Firebase.initializeApp();
+    FirebaseFirestore.instance.collection("posts").add(blogData).then((value) => print("Data Added")).catchError((e) {
+      print("Cannot add Data :$e");
+    });
+  }
+}
--- lib/views/create_post.dart
+++ lib/views/create_post.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:kirkit/services/crud.dart';
+import 'package:random_string/random_string.dart';
 
 class CreatePost extends StatefulWidget {
   @override
@@ -6,9 +12,44 @@ class CreatePost extends StatefulWidget {
 }
 
 class _CreatePostState extends State<CreatePost> {
-
+  bool isLoading = false;
+  CrudMethods crudMethods = CrudMethods();
   String title;
   String content;
+  File selectedImage;
+  final picker = ImagePicker();
+  Future getImage() async {
+    final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        selectedImage = File(pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  uploadPost() async{
+    if(selectedImage != null) {
+      setState(() {
+        isLoading = true;
+      });
+      StorageReference firebaseStorageRef = FirebaseStorage.instance.ref().child("images").child("${randomAlphaNumeric(9)}.jpg");
+      final StorageUploadTask task = firebaseStorageRef.putFile(selectedImage);
+      var downloadUrl = await(await task.onComplete).ref.getDownloadURL();
+      Map<String, String> postData;
+      postData = {
+      "imageUrl": downloadUrl,
+      "title" : title,
+      "content": content,
+      };
+      crudMethods.addData(postData);
+      Navigator.pop(context);
+    } else {
+      print('Image is not selected');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,21 +67,45 @@ class _CreatePostState extends State<CreatePost> {
         actions: <Widget>[
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
-            child: Icon(Icons.file_upload),
+            child: GestureDetector(
+              onTap: () {
+                uploadPost();
+              },
+              child: Icon(Icons.file_upload),
+            ),
           ),
         ],
       ),
-      body: Container(
+      body: isLoading
+          ? Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      )
+          : Container(
         child: ListView(
           children: <Widget>[
             SizedBox(height: 10,),
-            Container(
-              height: 150,
-              margin: EdgeInsets.symmetric(horizontal: 15.0),
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10),),
-              child: Icon(Icons.add_a_photo, color: Colors.black,),
-            ),
+            GestureDetector(
+              onTap: () {
+                getImage();
+              },
+              child: selectedImage == null ? Container(
+                height: 150,
+                margin: EdgeInsets.symmetric(horizontal: 15.0),
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(10),),
+                child: Icon(Icons.add_a_photo, color: Colors.black,),
+              ) : Container(
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.file(selectedImage, fit: BoxFit.cover,
+                      ),
+                  ),
+                  height: 150,
+                  margin: EdgeInsets.symmetric(horizontal: 15.0),
+                  width: MediaQuery.of(context).size.width,
+                ),
+              ),
             SizedBox(height: 10,),
             Padding(
               padding: const EdgeInsets.all(15.0),
